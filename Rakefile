f562946eccc602413ef5f5298d8ef4f6fd576e38
Sebastian Feuerstack <Sebastian@Feuerstack.de>
2011-07-28 09:34:51 -0300

    - initial commit

---
+++ Rakefile
@@ -0,0 +1,21 @@
+# -*- ruby -*-
+
+require 'rubygems'
+require 'hoe'
+
+# Hoe.plugin :compiler
+# Hoe.plugin :cucumberfeatures
+# Hoe.plugin :gem_prelude_sucks
+# Hoe.plugin :inline
+# Hoe.plugin :manifest
+Hoe.plugin :newgem
+# Hoe.plugin :racc
+# Hoe.plugin :rubyforge
+# Hoe.plugin :website
+
+Hoe.spec 'MINT-sdl' do
+  self.developer 'Sebastian Feuerstack', 'Sebastian@Feuerstack.org'
+  self.rubyforge_name       = self.name # TODO this is default value
+  self.extra_deps         = [['MINT-core','~> 0.0.1'],
+                             ['rubysdl',"~>2.1.1"]]
+end
---
+++ lib/MINT-sdl/sound.rb
@@ -0,0 +1,41 @@
+require 'rubygems'
+require 'MINT-core'
+require 'sdl'
+
+include MINT
+
+class SoundAgent < Agent
+
+   def initialize
+     SDL::init(SDL::INIT_AUDIO)
+     SDL::Mixer.open(22050, SDL::Mixer::DEFAULT_FORMAT, 2, 512)
+      @zoom = SDL::Mixer::Wave.load(File.join(File.dirname(__FILE__), "sounds", "click1.wav"))
+      @click2 = SDL::Mixer::Wave.load(File.join(File.dirname(__FILE__), "sounds","click.wav"))
+      @plop = SDL::Mixer::Wave.load(File.join(File.dirname(__FILE__), "sounds","plop.wav"))
+      @windup = SDL::Mixer::Wave.load(File.join(File.dirname(__FILE__), "sounds","windup.wav"))
+    super
+   end
+
+   def play_click(result)
+   # STDOUT.puts " in play"
+     SDL::Mixer.play_channel(0, @zoom, 0)     
+   end
+
+   def play_click2(result)
+   # STDOUT.puts " in play"
+     SDL::Mixer.play_channel(0, @click2, 0)     
+   end
+
+   def play_plop(result)
+     SDL::Mixer.play_channel(0, @plop, 0)     
+   end
+
+   def play_windup(result)
+     SDL::Mixer.play_channel(0, @windup, 0)     
+   end
+	
+
+end
+
+
+
